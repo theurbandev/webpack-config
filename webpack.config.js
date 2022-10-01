d8f10ff49a7408c0c8bf6cdabbc186724541181d
Matthew Joseph <matthew.j.joe@gmail.com>
2022-10-01 01:42:09 -0400

    tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,21 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./pages/test.js",
+  entry: "./pages/test.ts",
+  devtool: "inline-source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "test-bundle.js",
+    filename: "test-bundle.ts",
   },
   module: {
-    rules: [{ test: /\.ts$/, use: "ts-loader" }],
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
 };
